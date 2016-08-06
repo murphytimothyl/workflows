8f6418928d25a4dd557a99f8e138c1e44b574d4c
Tim Murphy <murphytimothyl@gmail.com>
2016-08-05 20:27:40 -0400

    streamlined variable definitions

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,16 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	compass = require('gulp-compass');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
+var coffeeSources = ['components/coffee/tagline.coffee'],
+	jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js',
-	];
-
-var sassSources = ['components/sass/style.scss'];
+	], 
+	sassSources = ['components/sass/style.scss'], 
+	htmlSources = ['builds/development/*.html'],
+    jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -48,6 +49,8 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json'])
 });
 
 gulp.task('connect', function() {
@@ -55,6 +58,16 @@ gulp.task('connect', function() {
 		root: 'builds/development',
 		livereload: true
 	});
+});
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
 });
 
 gulp.task('default', ['coffee', 'js', 'compass', 'watch', 'connect']);
